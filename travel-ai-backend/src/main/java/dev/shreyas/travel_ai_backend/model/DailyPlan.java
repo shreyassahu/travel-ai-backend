eeaea739eab99ffdaba1bb477f3371ffa359b8c3
shreyassahu <shreyassahu2208@gmail.com>
2025-08-18 01:07:45 -0400

    Just showing up

--- travel-ai-backend/src/main/java/dev/shreyas/travel_ai_backend/model/DailyPlan.java
+++ travel-ai-backend/src/main/java/dev/shreyas/travel_ai_backend/model/DailyPlan.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class   DailyPlan {
+public class DailyPlan {
   private int day;
   private List<Activity> activities;
   private Double cost;
